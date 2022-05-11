8b0d199e515c3d67fa04c732ebb3129b72cbf822
Lucas Miranda <lmiranda887@gmail.com>
2022-05-10 21:30:35 -0300

    Mapeando minhas entidades com JPA

--- src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/santander/banklineapi/model/Movimentacao.java
@@ -2,11 +2,31 @@ package com.dio.santander.banklineapi.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "tab_movimentacao")
+
 public class Movimentacao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(name = "data_hora")
 	private LocalDateTime dataHora; //Alterado para Date
+	
 	private String descricao;
+	
 	private Double valor;
+	
+	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo; //Corrigido erro
 	
 	public Integer getId() {
@@ -40,5 +60,4 @@ public class Movimentacao {
 		this.tipo = tipo;
 	}
 	
-
 }
