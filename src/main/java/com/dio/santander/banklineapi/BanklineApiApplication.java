35942b0375343f860ebe33490612aa14c1089456
Lucas Miranda <lmiranda887@gmail.com>
2022-05-10 23:23:14 -0300

    Correção senha e usuário / Criação tabela Postgres

--- src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/banklineapi/BanklineApiApplication.java
@@ -1,7 +1,9 @@
 package com.dio.santander.banklineapi;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class BanklineApiApplication {
