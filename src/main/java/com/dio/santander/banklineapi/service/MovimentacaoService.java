5e3807db993789a511875561dca2e7cfa1a4b1bc
Lucas Miranda <lmiranda887@gmail.com>
2022-05-14 12:56:17 -0300

    Atualizado Movimentacao no Swagger

---
+++ src/main/java/com/dio/santander/banklineapi/service/MovimentacaoService.java
@@ -0,0 +1,36 @@
+package com.dio.santander.banklineapi.service;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dio.santander.banklineapi.dto.NovaMovimentacao;
+import com.dio.santander.banklineapi.model.Movimentacao;
+import com.dio.santander.banklineapi.model.MovimentacaoTipo;
+import com.dio.santander.banklineapi.repository.MovimentacaoRepository;
+
+@Service
+public class MovimentacaoService {
+	@Autowired
+	private MovimentacaoRepository repository;
+	
+	public void save(NovaMovimentacao novaMovimentacao) {
+		
+		Movimentacao movimentacao = new Movimentacao();
+		
+		//Double valor = novaMovimentacao.getTipo() ==MovimentacaoTipo.RECEITA ? novaMovimentacao.getValor() : novaMovimentacao.getValor * -1;
+		
+		Double valor = novaMovimentacao.getValor();
+		if (novaMovimentacao.getTipo() == MovimentacaoTipo.DESPESA)
+			valor = valor * -1; 
+		
+		movimentacao.setDataHora(LocalDateTime.now());
+		movimentacao.setIdConta(novaMovimentacao.getIdConta());
+		movimentacao.setTipo(novaMovimentacao.getTipo());
+		movimentacao.setValor(valor);
+		
+		repository.save(movimentacao);
+		
+		}	
+}
